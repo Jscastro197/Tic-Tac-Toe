e8c7ad5d83a331a2a41f293315789198100ddbb3
SoldierMedic <pv2rojas@gmail.com>
2023-06-07 19:17:29 -0400

    added comments

--- minimax.py
+++ minimax.py
@@ -10,10 +10,10 @@ class Computer:
         game_state: Current state of the game as a list.
         """
         game_state_copy = game_state.copy()  # Create a copy of the game state
-        best_move = self.minimax(game_state_copy, self.letter)
+        best_move = self.minimax(game_state_copy, self.letter) # Get the best move for the computer
         return best_move['position']
 
-    def minimax(self, game_state, player):
+    def minimax(self, game_state, player): # Recursive function that returns the best move for the computer
         max_player = self.letter
         other_player = 'O' if player == 'X' else 'X'
 
@@ -22,13 +22,13 @@ class Computer:
         elif not self.num_empty_squares(game_state):
             return {'position': None, 'score': 0}
 
-        best = {'position': None, 'score': -math.inf if player == max_player else math.inf}
-        for possible_move in self.available_moves(game_state):
-            game_state[possible_move] = player
-            sim_score = self.minimax(game_state, other_player)
-            game_state[possible_move] = ''
+        best = {'position': None, 'score': -math.inf if player == max_player else math.inf} # Set the best score to -infinity if the current player is the computer, otherwise set it to infinity
+        for possible_move in self.available_moves(game_state): # Loop through all the available moves
+            game_state[possible_move] = player # Make the move
+            sim_score = self.minimax(game_state, other_player) # Get the score of the move
+            game_state[possible_move] = '' # Undo the move
 
-            sim_score['position'] = possible_move
+            sim_score['position'] = possible_move # Set the position of the move
 
             if (player == max_player and sim_score['score'] > best['score']) or (
                     player != max_player and sim_score['score'] < best['score']):
